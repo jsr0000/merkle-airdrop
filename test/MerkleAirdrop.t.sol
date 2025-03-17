05ea5763320ff08f952522feda1c30b9c9da8dbc
jsr0000 <josh.regnart@gmail.com>
2025-03-17 12:32:07 +0000

    scripts

---
+++ test/MerkleAirdrop.t.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+import {BagelToken} from "../src/BagelToken.sol";
+import {ZkSyncChainChecker} from "lib/foundry-devops/src/ZkSyncChainChecker.sol";
+import {DeployMerkleAirdrop} from "../script/DeployMerkleAirdrop.s.sol";
+
+contract MerkleAirdropTest is Test, ZkSyncChainChecker {
+    MerkleAirdrop public airdrop;
+    BagelToken public token;
+    address user;
+    uint256 userPrivateKey;
+
+    bytes32 public ROOT =
+        0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    uint256 public AMOUNT = 25 * 1e18;
+    uint256 public MINT_AMOUNT = AMOUNT * 4;
+    bytes32 proofOne =
+        0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 proofTwo =
+        0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] public PROOF = [proofOne, proofTwo];
+
+    function setUp() public {
+        if (!isZkSyncChain()) {
+            DeployMerkleAirdrop deployer = new DeployMerkleAirdrop();
+            (airdrop, token) = deployer.deployMerkleAirdrop();
+        } else {
+            token = new BagelToken();
+            airdrop = new MerkleAirdrop(ROOT, token);
+            token.mint(token.owner(), MINT_AMOUNT);
+            token.transfer(address(airdrop), MINT_AMOUNT);
+            (user, userPrivateKey) = makeAddrAndKey("user");
+        }
+    }
+
+    function testUsersCanClaim() public {
+        uint256 startingBalance = token.balanceOf(user);
+        console.log("startingBalance: ", startingBalance);
+        vm.prank(user);
+        airdrop.claim(user, AMOUNT, PROOF);
+        uint256 endingBalance = token.balanceOf(user);
+        console.log("endingBalance: ", endingBalance);
+        assertEq(endingBalance - startingBalance, AMOUNT);
+    }
+}
